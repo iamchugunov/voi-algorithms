f2675cd0663afdfa776b729f54bb13f736f7f655
Ерочкина Софья <116631335+SONIKS1@users.noreply.github.com>
2022-11-14 22:11:40 +0300

    Тупик в задании
    ввела функцию R для сравнения и подбора наиболее подходящей траектории для точки.
    Получаю ошибку на дебаге отследить не получилось ошибку.

--- matching/is_match_traj.m
+++ matching/is_match_traj.m
@@ -1,6 +1,12 @@
-function [flag] = is_match_traj(traj, poit, config)
-    % 1. Берем последнюю точку траектории
-    % 2. Рассчитываем расстояние между новой точкой и последней (2D или 3D)
-    % 3. Сравниваем полученную норму с каким-то порогом
-    % 4. Откуда взять порог
-end
+function [R] = is_match_traj(traj, poit, config, k)
+%вектор значений, по которому буду определять подходящую траекторию
+    for j = 1:k
+        last_poit = traj(j).poits(end);
+        new_crd = poit.est_crd;
+        delta = last_crd - new_crd;
+        Rnorm = norm(delta(1:2));
+        R.count = R.count + 1
+        R.poits(R.count) = Rnorm;
+    end
+
+
--- matching/matching_main.m
+++ matching/matching_main.m
@@ -1,29 +1,29 @@
 function [traj] = matching_main(poits, config)
-   
-    k = 0;
 
-    for i = 1:length(poits)
-
-        % рассчитываем координаты точки одномоментным способом
-        % проверяем рассчитались ли координаты
+k = 0;
+for i = 1:length(poits)
+    % рассчитываем координаты точки одномоментным способом
+    poits(i) = crd_calc(poits(i),config);
+    % проверяем рассчитались ли координаты
+    if (~poits(i).crd_valid)
         % если нет - точка в помойку
-        match_flag = 0;
-
-        for j = 1:k
-            if (is_match_traj(traj(j), poits(i), config))
-                traj(j) = add_poit_to_traj(traj(j),poits(i),config);
-                match_flag = 1;
-                break
-            end
-        end
-
-        if (match_flag)
-            continue
-        end
-
-        new_traj_ = new_traj(poits(i), config);
-        k = k + 1;
-        traj(k) = new_traj_;
-
+        continue
     end
+
+    match_flag = 0;
+    R = is_match_traj(traj, poits(i), config, k);
+    if(min(R.poits) < 3*config.sigma_n_ns + poits.h_geo)
+        match_flag=1;
+    end 
+
+    if (match_flag)
+        traj(index(min(R.poits))) = add_poit_to_traj(traj(index),poits(i),config);
+        continue
+    end
+
+    new_traj_ = new_traj(poits(i), config);
+    k = k + 1;
+    traj(k) = new_traj_;
+
+end
 end
