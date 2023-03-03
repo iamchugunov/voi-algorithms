d8226fced774a09adaf63d16abb91d2279cdc9c8
Irrelevant52 <62394895+Irrelevant52@users.noreply.github.com>
2023-03-03 05:55:54 +0300

    РФК с накоплением
    Написаны реализации для векторов:
    [X Y Z]
    [X Vx  Y Vy  Z Vz ]
    [X Vx ax Y Vy ay Z Vz az]

--- diploma/matrix_func/make_dS.m
+++
@@ -1,9 +0,0 @@
-function dS = make_dS(y, X, posts)
-    dS = zeros(length(y),length(X));
-    for i = 1:length(y)
-        d = sqrt((X(1,1) - posts(1,i))^2 + (X(4,1) - posts(2,i))^2 + (X(7,1) - posts(3,i))^2);
-        dS(i,1) = (X(1,1) - posts(1,i))/d;
-        dS(i,4) = (X(4,1) - posts(2,i))/d;
-        dS(i,7) = (X(7,1) - posts(3,i))/d;
-    end
-end
